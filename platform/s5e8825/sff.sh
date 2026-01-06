d6877e204c796be34b7b7b9c47feb132a33bb1cc
Majaahh <maja.blaszczak@proton.me>
2026-01-06 23:50:13 +0100

    a53x: commonise into a platform
    Change-Id: I4988be1c12e8dc2989bbdc5c67dd63af219678db
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ platform/s5e8825/sff.sh
@@ -0,0 +1,10 @@
+# Copyright (c) 2026 Majaahh
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# SEC Floating Feature configuration file for Exynos 1280 Devices (s5e8825)
+
+# Gallery
+SEC_FLOATING_FEATURE_GALLERY_CONFIG_IMAGE_TAGGER_VERSION=V701
+
+# Refresh Rate
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_MODE=2
