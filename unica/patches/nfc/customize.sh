ebe74b0e9146b6d13b2b23861b419effdda7c295
Majaahh <maja.blaszczak@proton.me>
2026-03-06 23:43:06 +0100

    unica: patches: nfc: rework NXP NFC patch
    Change-Id: Iee6ada495dff4840aba8d13e626cca8cd576a7ee
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- unica/patches/nfc/customize.sh
+++ unica/patches/nfc/customize.sh
@@ -36,34 +36,31 @@ fi
 # - API 35 and below: libnfc_nxpsn_jni.so
 # - API 36: libnfc_nci_jni.so
 if [ -f "$WORK_DIR/system/system/lib/libnfc_nci_jni.so" ]; then
-    if [ ! -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/lib/libnfc_nci_jni.so" ] && \
-            [ ! -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/lib64/libnfc_nxpsn_jni.so" ]; then
+    if [ ! -f "$WORK_DIR/vendor/lib64/nfc_nci_nxpsn.so" ]; then
         DELETE_FROM_WORK_DIR "system" "system/lib/libnfc_nci_jni.so"
         DELETE_FROM_WORK_DIR "system" "system/lib/libnfc_prop_extn.so"
         DELETE_FROM_WORK_DIR "system" "system/lib/libnfc_vendor_extn.so"
-    fi
-elif [ -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/lib/libnfc_nci_jni.so" ]; then
-     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libnfc_nci_jni.so" 0 0 644 "u:object_r:system_lib_file:s0"
-     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libnfc_prop_extn.so" 0 0 644 "u:object_r:system_lib_file:s0"
-     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libnfc_vendor_extn.so" 0 0 644 "u:object_r:system_lib_file:s0"
-elif [ -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/lib64/libnfc_nxpsn_jni.so" ]; then
-    # TODO
-    _LOG "Missing prebuilt blobs for NXP_SN100U NFC chip"
-fi
-if [ -f "$WORK_DIR/system/system/lib64/libnfc_nci_jni.so" ]; then
-    if [ ! -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/lib64/libnfc_nci_jni.so" ] && \
-            [ ! -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/lib64/libnfc_nxpsn_jni.so" ]; then
         DELETE_FROM_WORK_DIR "system" "system/lib64/libnfc_nci_jni.so"
         DELETE_FROM_WORK_DIR "system" "system/lib64/libnfc_prop_extn.so"
         DELETE_FROM_WORK_DIR "system" "system/lib64/libnfc_vendor_extn.so"
     fi
-elif [ -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/lib64/libnfc_nci_jni.so" ]; then
-     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libnfc_nci_jni.so" 0 0 644 "u:object_r:system_lib_file:s0"
-     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libnfc_prop_extn.so" 0 0 644 "u:object_r:system_lib_file:s0"
-     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libnfc_vendor_extn.so" 0 0 644 "u:object_r:system_lib_file:s0"
+elif [ -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/lib/libnfc_nci_jni.so" ]; then
+    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libnfc_nci_jni.so" 0 0 644 "u:object_r:system_lib_file:s0"
+    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libnfc_prop_extn.so" 0 0 644 "u:object_r:system_lib_file:s0"
+    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libnfc_vendor_extn.so" 0 0 644 "u:object_r:system_lib_file:s0"
 elif [ -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/lib64/libnfc_nxpsn_jni.so" ]; then
     # TODO
     _LOG "Missing prebuilt blobs for NXP_SN100U NFC chip"
+fi
+if [ ! -f "$WORK_DIR/system/system/lib64/libnfc_nci_jni.so" ]; then
+    if [ -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/lib64/libnfc_nci_jni.so" ]; then
+        ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libnfc_nci_jni.so" 0 0 644 "u:object_r:system_lib_file:s0"
+        ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libnfc_prop_extn.so" 0 0 644 "u:object_r:system_lib_file:s0"
+        ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libnfc_vendor_extn.so" 0 0 644 "u:object_r:system_lib_file:s0"
+    elif [ -f "$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/lib64/libnfc_nxpsn_jni.so" ]; then
+        # TODO
+        _LOG "Missing prebuilt blobs for NXP_SN100U NFC chip"
+    fi
 fi
 
 # SEC_PRODUCT_FEATURE_NFC_CHIP_NAME:=STM_ST21
