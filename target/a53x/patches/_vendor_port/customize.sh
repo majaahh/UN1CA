dbbfbc747e99191f476474c1fb2fca00eace716f
Majaahh <maja.blaszczak@proton.me>
2026-03-16 15:45:43 +0100

    target: a53x: vendor_port: Adapt build.prop
    Change-Id: Ie21cd44956447f856a21cc900a6395cf219b8feb
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- target/a53x/patches/_vendor_port/customize.sh
+++ target/a53x/patches/_vendor_port/customize.sh
@@ -31,4 +31,65 @@ LOG_STEP_OUT
 # shellcheck disable=SC2046
 wait $(jobs -p) || return 1
 
+LOG_STEP_IN "- Adapting /vendor/build.prop"
+# Codec2
+SET_PROP "vendor" "debug.codec2.stop_hal_before_surface" "1"
+SET_PROP "vendor" "debug.sf.disable_backpressure" "1"
+SET_PROP "vendor" "debug.stagefright.c2-poolmask" --delete
+SET_PROP "vendor" "debug.stagefright.ccodec_lax_type" --delete
+SET_PROP "vendor" "debug.stagefright.ccodec_strict_type" --delete
+SET_PROP "vendor" "vendor.debug.c2.sbwc.enable" --delete
+
+# DRM
+SET_PROP "vendor" "ro.netflix.bsp_rev" "EXYNOS1280-34993-1"
+
+# Device
+SET_PROP "vendor" "ro.product.vendor.device" "a53x"
+SET_PROP "vendor" "ro.product.vendor.model" "a53x"
+SET_PROP "vendor" "ro.product.vendor.name" "a53xnaxx"
+
+# Fips
+SET_PROP "vendor" "ro.security.fips_fmp.ver" "4.0"
+SET_PROP "vendor" "ro.security.fips_scrypto.ver" "2.6"
+SET_PROP "vendor" "ro.security.fips_skc.ver" "2.3"
+
+# Graphics
+SET_PROP "vendor" "ro.vendor.ddk.set.afbc" "true"
+
+# HWC
+SET_PROP "vendor" "vendor.hwc.exynos.vsync_mode" --delete
+
+# Keystore
+SET_PROP "vendor" "ro.security.keystore.keytype" "sakv2,gak,"
+
+# Platform
+SET_PROP "vendor" "ro.board.platform" "universal8825"
+SET_PROP "vendor" "ro.product.board" "s5e8825"
+SET_PROP "vendor" "ro.soc.model" "s5e8825"
+
+# Ramdisk
+SET_PROP "vendor" "ro.config.pageboost.vramdisk.bootfile.enabled" "true"
+
+# SLMK
+SET_PROP "vendor" "ro.slmk.2nd.dha_cached_min" "4"
+SET_PROP "vendor" "ro.slmk.2nd.dha_empty_max" "30"
+SET_PROP "vendor" "ro.slmk.2nd.swap_free_low_percentage" "20"
+SET_PROP "vendor" "ro.slmk.bEFKb_enable" "true"
+SET_PROP "vendor" "ro.slmk.beks_key" "431"
+SET_PROP "vendor" "ro.slmk.dha_empty_max" "24"
+SET_PROP "vendor" "ro.slmk.dha_pwhl_chn_key" "1548"
+SET_PROP "vendor" "ro.slmk.dha_th_rate" "3.5"
+SET_PROP "vendor" "ro.slmk.freelimit_val" "14"
+SET_PROP "vendor" "ro.slmk.plg_key" "9220"
+SET_PROP "vendor" "ro.slmk.psi_critical" "120"
+SET_PROP "vendor" "ro.slmk.swap_free_low_percentage" "40"
+SET_PROP "vendor" "ro.slmk.v_bonusEFK" "64512"
+
+# System
+SET_PROP "vendor" "sys.perf.hmp" "6:2"
+
+# VPNPP
+SET_PROP "vendor" "ro.security.vpnpp.release" "1.0"
+LOG_STEP_OUT
+
 unset TARGET_EXTRA_FIRMWARE_DIR MODEL REGION
