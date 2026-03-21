17f3e57f02ead50c4284a44a2b10a752e660a3ee
Majaahh <maja.blaszczak@proton.me>
2026-03-21 15:12:09 +0100

    platform: s5e8825: audio: skip extra audio blobs replacement for m33x
    Change-Id: Ifb3e5edf415cedbe6fe897774554cd68a02ebaf3
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- platform/s5e8825/patches/audio/customize.sh
+++ platform/s5e8825/patches/audio/customize.sh
@@ -3,7 +3,7 @@ DELETE_FROM_WORK_DIR "system" "system/lib64/lib_SoundAlive_play_plus_ver800.so"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libsamsungSoundbooster_plus_legacy.so" 0 0 644 "u:object_r:system_lib_file:s0"
 
-if [[ "$TARGET_CODENAME" != "m34x" ]]; then
+if [[ "$TARGET_CODENAME" != "m33x" ]] && [[ "$TARGET_CODENAME" != "m34x" ]]; then
     LOG "\033[0;33m! Skipping extra blob replacement\033[0m"
     return 0
 fi
